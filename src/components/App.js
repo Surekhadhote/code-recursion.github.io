62da66921c58b574f15037fca40b4848984e7d92
Ajay Singh <helloajaysingh1@gmail.com>
2020-09-13 12:08:24 +0530

    added css for responsive styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import "../styles/responsive.css";
 import Navbar from "./Navbar";
 import About from "./About";
 import Project from "./Project";
